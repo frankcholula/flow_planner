a8492937f08a5ad7c88295ddd0407e19c146951a
frankcholula <lu.phrank@gmail.com>
2025-07-30 15:00:30 +0100

    getting another run in.

--- src/experiments/lunarlander/cfm_start_obs.sh
+++ src/experiments/lunarlander/cfm_start_obs.sh
@@ -1,14 +1,32 @@
+#!/bin/bash
 python -m src.run \
     --environment "LunarLander-v3" \
     --horizon 100 \
     --batch-size 32 \
-    --num-epochs 1000 \
+    --num-epochs 100 \
     --print-every 10 \
     --lr 1e-3 \
     --model-type "ccnn" \
-    --kernel-size 5 \
+    --kernel-size 10 \
     --hidden-dim 128 \
     --step-size 0.05 \
     --solver-method "midpoint" \
     --inference-batch-size 1 \
     --condition-on "start_obs"
+
+
+# uncomment for using MLP
+# python -m src.run \
+#     --environment "LunarLander-v3" \
+#     --horizon 100 \
+#     --batch-size 32 \
+#     --num-epochs 1000 \
+#     --print-every 10 \
+#     --lr 1e-3 \
+#     --model-type "mlp" \
+#     --kernel-size 5 \
+#     --hidden-dim 128 \
+#     --step-size 0.05 \
+#     --solver-method "midpoint" \
+#     --inference-batch-size 1 \
+#     --condition-on "start_obs"
