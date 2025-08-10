5dd5fd60abfeb64f3789b4bf267e014ecbda4ff9
frankcholula <lu.phrank@gmail.com>
2025-08-10 17:54:26 +0100

    removed unnecessary params.

--- src/experiments/bipedalwalker/train_zoo.sh
+++ src/experiments/bipedalwalker/train_zoo.sh
@@ -3,19 +3,7 @@
 python -m rl_zoo3.train \
     --algo ppo \
     --env BipedalWalker-v3 \
-    --n-timesteps 5000000 \
     --track \
     --wandb-project-name "Flow Planner" \
     --wandb-entity frankcholula \
-    --device cpu \
-    --hyperparams \
-    normalize:True \
-    n_envs:32 \
-    n_steps:2048 \
-    batch_size:64 \
-    gae_lambda:0.95 \
-    gamma:0.999 \
-    n_epochs:10 \
-    ent_coef:0.0 \
-    learning_rate:3e-4 \
-    clip_range:0.18
+    --device cpu
--- src/experiments/lunarlander/train_zoo.sh
+++ src/experiments/lunarlander/train_zoo.sh
@@ -3,16 +3,7 @@
 python -m rl_zoo3.train \
     --algo ppo \
     --env LunarLanderContinuous-v3 \
-    --n-timesteps 1000000 \
     --track \
     --wandb-project-name "Flow Planner" \
     --wandb-entity frankcholula \
-    --device cpu \
-    --hyperparams \
-    n_envs:16 \
-    n_steps:1024 \
-    batch_size:64 \
-    n_epochs:4 \
-    gae_lambda:0.98 \
-    gamma:0.999 \
-    ent_coef:0.01
+    --device cpu
