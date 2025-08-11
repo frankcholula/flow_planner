823fb7fa3be77a03c1df176799018291be782b16
frankcholula <lu.phrank@gmail.com>
2025-08-12 00:04:58 +0100

    try ppo_lstm.

--- src/experiments/carracing/train_zoo.sh
+++ src/experiments/carracing/train_zoo.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 # hyperparameters taken from https://github.com/DLR-RM/rl-baselines3-zoo/blob/ab4aadb57c6c42abcf1016318c2bebe35c4c1270/hyperparams/ppo.yml#L350-L379
 
+# python -m rl_zoo3.train \
+#     --algo ppo \
+#     --env CarRacing-v3 \
+#     --track \
+#     --wandb-project-name "Flow Planner" \
+#     --wandb-entity frankcholula \
+#     --device auto
+
 python -m rl_zoo3.train \
-    --algo ppo \
+    --algo ppo_lstm \
     --env CarRacing-v3 \
     --track \
     --wandb-project-name "Flow Planner" \
