872503c2d08c62c3b100f372c3d41ce66498cdb6
frankcholula <lu.phrank@gmail.com>
2025-08-14 18:18:58 +0100

    changed envrionment.

--- src/conf/environment.py
+++ src/conf/environment.py
@@ -3,22 +3,33 @@ from dataclasses import dataclass
 
 @dataclass
 class LunarLanderConfig:
-    # TODO: refactor this to data collector
-    # policy_type: str = "MlpPolicy"
-    # total_timesteps: int = int(10e5)
     env_name: str = "LunarLander-v3"
-    dataset_name: str = "LunarLanderContinuous-v3/ppo-1000-deterministic-v1"
+    dataset_name: str = "Box2D/LunarLanderContinuous-v3/expert-v0"
     obs_dim: int = 8
     action_dim: int = 2
 
+@dataclass
+class CarRacingConfig:
+    env_name: str = "CarRacing-v3"
+    dataset_name: str = "Box2D/CarRacing-v3/expert-v0"
+    obs_dim: int = 64
+    action_dim: int = 3
 
+@dataclass
+class BipedalWalkerConfig:
+    env_name: str = "BipedalWalker-v3"
+    dataset_name: str = "Box2D/BipedalWalker-v3/expert-v0"
+    obs_dim: int = 24
+    action_dim: int = 4
+
+@dataclass
 class HopperConfig:
     env_name: str = "Hopper-v5"
     dataset_name: str = "mujoco/hopper/expert-v0"
     obs_dim: int = 11
     action_dim: int = 3
 
-
+@dataclass
 class Walker2dConfig:
     env_name: str = "Walker2d-v5"
     dataset_name: str = "mujoco/walker2d/expert-v0"
