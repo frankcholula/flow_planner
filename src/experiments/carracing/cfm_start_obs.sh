812f768aad1aadc496215f85865c7121ce4e2bd1
Frank Lu <lu.phrank@gmail.com>
2025-08-17 23:47:21 +0100

    added new model and change running.

---
+++ src/experiments/carracing/cfm_start_obs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# ccnn
+# python -m src.run \
+#     --environment CarRacing-v3 \
+#     --horizon 30 \
+#     --batch-size 64 \
+#     --num-epochs 100 \
+#     --model-type ccnn \
+#     --condition-on start_obs \
+#     --chunk-type obs_only \
+#     --lr 1e-3
+
+# unet
+python -m src.run \
+    --environment CarRacing-v3 \
+    --horizon 30 \
+    --batch-size 64 \
+    --num-epochs 100 \
+    --model-type unet \
+    --condition-on start_obs \
+    --chunk-type obs_only \
+    --lr 1e-3
--- src/experiments/carracing/train_global.sh
+++
@@ -1,9 +0,0 @@
-python -m src.run \
-    --environment CarRacing-v3 \
-    --horizon 30 \
-    --batch-size 64 \
-    --num-epochs 100 \
-    --model-type ccnn \
-    --condition-on start_obs \
-    --chunk-type obs_only \
-    --lr 1e-3
