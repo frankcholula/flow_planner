cbec99324d25e3d2f588dbb7112075357e6ddc78
Frank Lu <lu.phrank@gmail.com>
2025-08-17 12:24:30 +0100

    used mixed.

--- src/experiments/carracing/vae.sh
+++ src/experiments/carracing/vae.sh
@@ -3,4 +3,5 @@ python -m src.pipelines.carracing.vae_training \
     --epochs 50 \
     --lr 1e-3 \
     --latent_dim 32 \
-    --beta 1.0
+    --beta 1.0 \
+    --dataset_name Box2D/CarRacing-v3/mixed-v0
