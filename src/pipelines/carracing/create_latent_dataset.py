504ed63af6fddc265edb0688f7db80b1495f712c
frankcholula <lu.phrank@gmail.com>
2025-08-17 12:34:24 +0100

    refactor mixed dataset.

--- src/pipelines/carracing/create_latent_dataset.py
+++ src/pipelines/carracing/create_latent_dataset.py
@@ -10,28 +10,6 @@ import torch
 from torchvision import transforms
 
 
-def create_mixed_carracing_dataset():
-    EXPERT_ID = "Box2D/CarRacing-v3/expert-v0"
-    SIMPLE_ID = "Box2D/CarRacing-v3/simple-v0"
-    COMBINED_ID = "Box2D/CarRacing-v3/mixed-v0"
-
-    local_datasets = minari.list_local_datasets()
-    assert EXPERT_ID in local_datasets, f"Dataset `{EXPERT_ID}` not found."
-    assert SIMPLE_ID in local_datasets, f"Dataset `{SIMPLE_ID}` not found."
-    expert_dataset = minari.load_dataset(EXPERT_ID)
-    simple_dataset = minari.load_dataset(SIMPLE_ID)
-    print(
-        f"Combining {len(expert_dataset.episode_indices)} expert and {len(simple_dataset.episode_indices)} simple episodes..."
-    )
-    combined_dataset = minari.combine_datasets(
-        datasets_to_combine=[expert_dataset, simple_dataset],
-        new_dataset_id=COMBINED_ID,
-    )
-    print(
-        f"Successfully created '{COMBINED_ID}' with {len(combined_dataset.episode_indices)} total episodes."
-    )
-
-
 def create_env(vae_model_path, latent_dim, device, input_dim=64):
     base_env = gym.make("CarRacing-v3", render_mode="rgb_array", continuous=True)
     minari_env = YAMLCompatResizeObservation(env=base_env, shape=[input_dim, input_dim])
@@ -100,5 +78,4 @@ def create_latent_dataset(vae_model_path, latent_dim=32):
 
 if __name__ == "__main__":
     VAE_MODEL_PATH = "src/checkpoints/CarRacing-v3_vae_e50_l32_mixed.pth"
-    # create_mixed_carracing_dataset()
     create_latent_dataset(VAE_MODEL_PATH)
