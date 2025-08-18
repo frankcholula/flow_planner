ad3df7c76bd0b50a4d407600888bd3277de46ee1
frankcholula <lu.phrank@gmail.com>
2025-08-18 20:28:03 +0100

    separate the experiments.

--- src/experiments/lunarlander/fm_cnn.sh
+++ src/experiments/lunarlander/fm_cnn.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
+
+# uncomment for using MLP
 python -m src.run \
     --environment "LunarLander-v3" \
     --horizon 100 \
     --batch-size 32 \
-    --num-epochs 100 \
+    --num-epochs 1000 \
     --print-every 10 \
+    --eval-every 10 \
     --lr 1e-3 \
-    --model-type "ccnn" \
-    --kernel-size 10 \
+    --model-type "mlp" \
+    --kernel-size 5 \
     --hidden-dim 128 \
     --step-size 0.05 \
     --solver-method "midpoint" \
     --inference-batch-size 1 \
-    --condition-on "start_obs"
