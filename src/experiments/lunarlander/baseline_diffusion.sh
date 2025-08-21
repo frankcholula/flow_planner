fbeba66136bd6f9d48ee7bb4162b3e26329e5864
frankcholula <lu.phrank@gmail.com>
2025-08-21 08:40:12 +0100

    lets try running this.

--- src/experiments/lunarlander/baseline_diffusion.sh
+++ src/experiments/lunarlander/baseline_diffusion.sh
@@ -4,9 +4,9 @@ python -m src.diffusion \
     --environment "LunarLander-v3" \
     --horizon 100 \
     --batch-size 32 \
-    --num-epochs 10 \
-    --print-every 1 \
-    --eval-every 1 \
+    --num-epochs 100 \
+    --print-every 5 \
+    --eval-every 10 \
     --lr 1e-3 \
     --model-type "unet" \
     --hidden-dim 64 \
