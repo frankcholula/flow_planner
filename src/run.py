2ec604cd7ab29dbddb8cb0932bb8e4c3e2ec2a9b
Frank Lu <lu.phrank@gmail.com>
2025-08-21 16:49:46 +0100

    slight revision.

--- src/run.py
+++ src/run.py
@@ -236,7 +236,7 @@ def main():
     cond = args.condition_on if args.condition_on else "none"
     run_name = (
         f"{args.environment}_{args.model_type}_h{args.horizon}_e{args.num_epochs}_"
-        f"k{args.kernel_size}_chunk-{args.model_target}_cond-{cond}"
+        f"k{args.kernel_size}_target-{args.model_target}_cond-{cond}"
     )
     logger = None
     if not args.no_wandb:
