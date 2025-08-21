04250aad4dbb85fcee19e5a457bae5302bbbe731
frankcholula <lu.phrank@gmail.com>
2025-08-21 08:41:18 +0100

    pretty print.

--- src/diffusion.py
+++ src/diffusion.py
@@ -233,7 +233,7 @@ def train(config, args, dataset, env, run_name=None, logger=None):
     print("Dataset configuration:")
     pp.pprint(config)
     print("Training parameters:")
-    pp.pprint(args)
+    pp.pprint(vars(args))
     print("Starting training...")
 
     for epoch in range(args.num_epochs):
