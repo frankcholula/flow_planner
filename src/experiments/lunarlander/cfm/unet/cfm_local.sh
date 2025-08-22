3c3279f243c62916a52866a402db819b787533f0
Frank Lu <lu.phrank@gmail.com>
2025-08-22 09:09:22 +0100

    trying this out.

--- src/experiments/lunarlander/cfm/unet/cfm_local.sh
+++ src/experiments/lunarlander/cfm/unet/cfm_local.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+# set eval-every to bypass eval because there is no obs.
 python -m src.run \
     --environment "LunarLander-v3" \
     --horizon 25 \
     --batch-size 32 \
-    --num-epochs 100 \
+    --num-epochs 1 \
     --print-every 1 \
-    --eval-every 5 \
+    --eval-every 500 \
     --lr 1e-3 \
     --model-type "unet" \
     --hidden-dim 64 \
@@ -14,4 +15,3 @@ python -m src.run \
     --inference-batch-size 1 \
     --condition-on "start_obs_goal" \
     --model-target "act_only"
-    
