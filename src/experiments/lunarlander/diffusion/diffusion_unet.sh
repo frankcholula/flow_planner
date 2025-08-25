f80eb8c6f5bbf63fffdd980ced8b92de846ddc5e
Frank Lu <lu.phrank@gmail.com>
2025-08-25 19:31:20 +0100

    new model who dis.

--- src/experiments/lunarlander/diffusion/diffusion_unet.sh
+++ src/experiments/lunarlander/diffusion/diffusion_unet.sh
@@ -3,13 +3,13 @@ python -m src.diffusion \
     --environment "LunarLander-v3" \
     --horizon 100 \
     --batch-size 32 \
-    --num-epochs 5 \
+    --num-epochs 100 \
     --print-every 1 \
-    --eval-every 1 \
-    --lr 1e-3 \
+    --eval-every 5 \
+    --lr 1e-4 \
     --model-type "unet" \
     --hidden-dim 64 \
-    --scheduler "ddim" \
+    --scheduler "ddpm" \
     --num-train-timesteps 1000 \
     --num-inference-steps 100 \
     --inference-batch-size 1
