773a4951a63dfb0cae6ce046121c64b8ced536bf
frankcholula <lu.phrank@gmail.com>
2025-08-30 12:36:34 +0100

    added experiment.

--- src/experiments/lunarlander/cfm/unet/cfm_obs_act_start_obs_waypoint.sh
+++ src/experiments/lunarlander/cfm/unet/cfm_obs_act_start_obs_waypoint.sh
@@ -12,5 +12,5 @@ python -m src.run \
     --step-size 0.05 \
     --solver-method "midpoint" \
     --inference-batch-size 1 \
-    --condition-on "start_obs_goal" \
+    --condition-on "start_obs_waypoint" \
     --model-target "obs_act"
